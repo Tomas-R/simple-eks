4b6a606b2c9a6fbec5af4d54af9e104855bb050c
Tomas-R <raulickis.tomas@gmail.com>
2021-09-06 14:08:32 +0000

    create cluster, add 3 nodes; define a sample POD

--- lib/simple-eks-stack.ts
+++ lib/simple-eks-stack.ts
@@ -1,9 +1,39 @@
 import * as cdk from '@aws-cdk/core';
 
 export class SimpleEksStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {   // this is auto-generated
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    
+
+    // provisiong a cluster
+    const cluster = new eks.Cluster(this, 'hello-eks', {
+      version: eks.KubernetesVersion.V1_21,
+      defaultCapacity: 3    // number of nodes in a Managed Nodegroup
+    });
+
+    // apply a kubernetes manifest to the cluster
+    cluster.addManifest('mypod', {
+      apiVersion: 'v1',
+      kind: 'Pod',
+      metadata: { name: 'mypod' },
+      spec: {
+        containers: [
+          {
+            name: 'hello',
+            image: 'paulbouwer/hello-kubernetes:1.5',
+            ports: [ { containerPort: 8080 } ]
+          }
+        ]
+      }
+    });
+
+
+
+
+
+
+
+
   }
 }
